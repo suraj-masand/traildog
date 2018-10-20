cba675a05882440f01301d723a08a132de56a0e5
Jet171 <34073472+Jet171@users.noreply.github.com>
2018-10-20 01:22:32 -0400

    renamed Loginactivity to RegistrationActivity

--- app/src/main/java/com/traildog/app/RegistrationActivity.java
+++ app/src/main/java/com/traildog/app/RegistrationActivity.java
@@ -1,6 +1,5 @@
 package com.traildog.app;
 
-import android.app.ProgressDialog;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +8,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,9 +16,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button buttonRegister;
     private EditText editTextEmail;
@@ -34,7 +31,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
 
 
         firebaseAuth = FirebaseAuth.getInstance();
@@ -70,11 +67,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             //user registeered successfully
-                            Toast.makeText(LoginActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegistrationActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
                         } else {
-                            Toast.makeText(LoginActivity.this, "Could not register user, try again", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegistrationActivity.this, "Could not register user, try again", Toast.LENGTH_SHORT).show();
                             FirebaseAuthException e = (FirebaseAuthException )task.getException();
-                            Log.e("LoginActivity", "Failed Registration", e);
+                            Log.e("RegistrationActivity", "Failed Registration", e);
                         }
                     }
                 });
