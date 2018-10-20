4d990a25394efee702d7bb1c0899875c23482e05
Jet171 <34073472+Jet171@users.noreply.github.com>
2018-10-20 17:15:52 -0400

    instantiated treats in HomeScreen

--- app/src/main/java/com/traildog/app/HomeScreen.java
+++ app/src/main/java/com/traildog/app/HomeScreen.java
@@ -7,25 +7,72 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import com.traildog.app.model.TREATTYPE;
+import com.traildog.app.model.Treats;
 
 
 public class HomeScreen extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+    private Treats activeWearTreat;
+    private ImageView activeWearImageView;
+    private Treats donutTreat;
+    private ImageView donutImageView;
+    private Treats hockeyTreat;
+    private ImageView hockeyImageView;
+    private Treats italianFoodTreat;
+    private Treats lampLightTreat;
+    private Treats musicTreat;
+    private Treats popcornTreat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
         firebaseAuth = FirebaseAuth.getInstance();
+
+        //NOTE: these items are already in wallet
+        italianFoodTreat = new Treats(TREATTYPE.COUPON, 4,
+                "Two free side dishes of your choice " +
+                        "with the purchase of an extra-large pizza",
+                "drawable/italianfood.png");
+        lampLightTreat = new Treats(TREATTYPE.EVENT, 5, "", "drawable/lamplight.png");
+        popcornTreat = new Treats(TREATTYPE.COUPON, 7, "One free popcorn refill", "drawable/popcorn.png");
+
+
+        //NOTE: these following items are geotagged with location and longitude
+        //creates activeWear at campanile with id = 1 and TREATTYPE = COUPON
+        activeWearTreat = new Treats(TREATTYPE.COUPON, 1,
+                "$10.00", "drawable/activeWear.png", 33.7743,
+                84.3982, 200);
+
+        //creates donut treat at crc id = 2 TREATTYPE = COUPON
+        donutTreat = new Treats(TREATTYPE.COUPON, 2, "50%",
+                "drawable/donut.png", 33.7757,
+                84.4040, 200);
+
+        //creates hockey treat at klaus id = 3 TREATTYPE = EVENT
+        hockeyTreat = new Treats(TREATTYPE.EVENT, 3,
+                "",  "drawable/hockey.png",
+                33.7773, 84.3962, 200);
+
+        //creates music treat at library id = 6 TREATTYPE = EVENT
+        musicTreat = new Treats(TREATTYPE.EVENT, 6,
+                "October 29th", "drawable/music.png",
+                33.7743, 84.3957, 200);
+
+
+
+
         if(firebaseAuth.getCurrentUser() == null) {
             finish();
             startActivity(new Intent(this, SigninActivity.class));
         }
-
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
 
