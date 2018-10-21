4b3ce55cd2a653e66b24d2aad96be18a64a70a9d
Justin Higgins <just1higg@yahoo.com>
2018-10-21 04:07:27 -0400

    Wallet work.

--- app/src/main/java/com/traildog/app/Wallet.java
+++ app/src/main/java/com/traildog/app/Wallet.java
@@ -4,18 +4,35 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.traildog.app.model.Treats;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Wallet extends AppCompatActivity {
+    private List<Treats> list = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private WalletAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        recyclerView = findViewById(R.id.recycler_view);
+        mAdapter = new WalletAdapter(list);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,4 +43,7 @@ public class Wallet extends AppCompatActivity {
         });
     }
 
+    private void getTreats() {
+
+    }
 }
--- app/src/main/java/com/traildog/app/WalletAdapter.java
+++ app/src/main/java/com/traildog/app/WalletAdapter.java
@@ -1,4 +1,48 @@
 package com.traildog.app;
 
-public class WalletAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.traildog.app.model.Treats;
+
+import java.util.List;
+
+public class WalletAdapter extends RecyclerView.Adapter<WalletAdapter.MyViewHolder> {
+    private List<Treats> list;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView value, something;
+        public MyViewHolder(View view) {
+            super(view);
+            value = view.findViewById(R.id.row_value);
+            something = view.findViewById(R.id.row_something);
+        }
+    }
+
+    public WalletAdapter(List<Treats> list) {
+        this.list = list;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.wallet_row, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Treats treat = list.get(position);
+        holder.value.setText(treat.getValue());
+        holder.something.setText("something");
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
 }
+
