087c7d614686856af9866cb8891f27973e14331f
smasand6 <smasand6@gatech.edu>
2018-10-20 20:12:32 -0400

    attempt to fix launch errors

--- app/src/main/java/com/traildog/app/MainActivity.java
+++ app/src/main/java/com/traildog/app/MainActivity.java
@@ -1,98 +1,98 @@
-package com.traildog.app;
-
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
-import io.radar.sdk.Radar;
-
-
-public class MainActivity extends AppCompatActivity {
-
-    final String RADAR_KEY_TYPE = "radar-api-test-key";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_assemble);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        String radarKey = null;
-        Context context = getApplicationContext();
-        ApplicationInfo appInfo = null;
-        try {
-            appInfo = context.getPackageManager().getApplicationInfo(context.getPackageName(),PackageManager.GET_META_DATA);
-            radarKey = appInfo.metaData.getString(RADAR_KEY_TYPE);
-            Radar.initialize(radarKey);
-            Radar.setUserId("demoUser"); // change to UUID for currently logged in user
-            Radar.setDescription("A demo user.");
-
-//            if (Build.VERSION.SDK_INT >= 23) {
-            int requestCode = 0;
-            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, requestCode);
+//package com.traildog.app;
+//
+//import android.Manifest;
+//import android.content.Context;
+//import android.content.pm.ApplicationInfo;
+//import android.content.pm.PackageManager;
+//import android.location.Location;
+//import android.os.Build;
+//import android.os.Bundle;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
+//import android.support.v4.app.ActivityCompat;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.view.View;
+//
+//import io.radar.sdk.Radar;
+//
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    final String RADAR_KEY_TYPE = "radar-api-test-key";
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_assemble);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
 //            }
-
-//            Radar.trackOnce(new RadarCallback() {
-//                @Override
-//                public void onComplete(Radar.RadarStatus status, Location location, RadarEvent[] events, RadarUser user) {
-//                    // do something with status, location, events, user
-//                    if (status == Radar.RadarStatus.SUCCESS) {
-//                        if (events.length > 0) {
-//                            for (RadarEvent e : events) {
-//                                String result = RadarUtils.handleRadarEvent(e);
-//                            }
-//                        }
-//                    }
-//                }
-//            });
-            Radar.startTracking();
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-}
+//        });
+//
+//        String radarKey = null;
+//        Context context = getApplicationContext();
+//        ApplicationInfo appInfo = null;
+//        try {
+//            appInfo = context.getPackageManager().getApplicationInfo(context.getPackageName(),PackageManager.GET_META_DATA);
+//            radarKey = appInfo.metaData.getString(RADAR_KEY_TYPE);
+//            Radar.initialize(radarKey);
+//            Radar.setUserId("demoUser"); // change to UUID for currently logged in user
+//            Radar.setDescription("A demo user.");
+//
+////            if (Build.VERSION.SDK_INT >= 23) {
+//            int requestCode = 0;
+//            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, requestCode);
+////            }
+//
+////            Radar.trackOnce(new RadarCallback() {
+////                @Override
+////                public void onComplete(Radar.RadarStatus status, Location location, RadarEvent[] events, RadarUser user) {
+////                    // do something with status, location, events, user
+////                    if (status == Radar.RadarStatus.SUCCESS) {
+////                        if (events.length > 0) {
+////                            for (RadarEvent e : events) {
+////                                String result = RadarUtils.handleRadarEvent(e);
+////                            }
+////                        }
+////                    }
+////                }
+////            });
+//            Radar.startTracking();
+//        } catch (PackageManager.NameNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//}
